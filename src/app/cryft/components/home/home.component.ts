2fc09a29f4d16a98b3846e2000919630dd22ea6c
Nisha Poswal <nishaposwal321@gamil.com>
2022-01-29 09:21:09 +0530

    Add tnc page

--- src/app/cryft/components/home/home.component.ts
+++ src/app/cryft/components/home/home.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { EventEmitterService } from 'src/app/core/services/event-emitter.service';
 
@@ -21,7 +22,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
   @ViewChild('faq', { static: false })
   faq: ElementRef<HTMLElement> = {} as ElementRef;
   subscription: any = [];
-  constructor(private eventEmitterService: EventEmitterService, private authService : AuthService) {}
+  constructor(
+    private eventEmitterService: EventEmitterService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router : Router
+  ) {}
 
   ngOnInit(): void {
     this.subscription.push(
@@ -46,11 +52,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
             break;
         }
       })
-    ); 
-
-    this.authService.getServerProfile().subscribe(res=>{
+    );
+    this.authService.getServerProfile().subscribe((res) => {
       this.authService.setProfile(res);
-    })
+    });
+
+    this.route.queryParams.subscribe(params => {
+      console.log(this.router.getCurrentNavigation()?.extras)
+      if (this.router.getCurrentNavigation()?.extras.state?.scrollTo == 'buy_gift') {
+        this.eventEmitterService.emit({ type: 'BUY_GIFT', data: {} });
+          } else if(this.router.getCurrentNavigation()?.extras.state?.scrollTo == 'faq'){
+            this.eventEmitterService.emit({ type: 'FAQ', data: {} });
+          }
+        });
   }
 
   ngAfterViewInit() {}
