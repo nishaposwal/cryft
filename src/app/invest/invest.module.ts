e65b5891116fb39205063da3e910dc114752e540
Nisha Poswal <nishaposwal321@gamil.com>
2022-01-30 16:30:10 +0530

    Add inves module

---
+++ src/app/invest/invest.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InvestRoutingModule } from './invest-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InvestRoutingModule
+  ]
+})
+export class InvestModule { }
